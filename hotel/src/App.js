add76be637d6b2f4b2607446dc7b9ec370b56d6f
dhruvi kakadiya <dhruvikakakadiya0@gmail.com>
2025-01-28 11:43:31 +0530

    install tailwind

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+
+
+
+
 function App() {
   return (
     <div className="App">
--- hotel/src/components/Button.jsx
+++ hotel/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
 function Button() {
   return (
     <div>
-        <button>Click me!</button>
+      <button class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+  Click Me
+</button>
         </div>
   )
 }
