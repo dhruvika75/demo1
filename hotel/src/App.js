06f780071ef29fcb8b10a30d7bca7ce63ef37714
Dilip suthar <dilipkumarsuthar176@gmail.com>
2025-01-28 14:56:27 +0530

    headersection

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 import Button from './components/Button';
 
+import Header from './components/Header';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     <h1>hellowjhcidvsvnhnx </h1>
     <Button/>
     </div>
